244bb79967abe9ef5aee50f21e2064fbbca01e6c
Pratik <pratik.vernekar@robosoftin.com>
2023-01-19 10:14:35 +0530

    installation

---
+++ src/components/cards/Cards.js
@@ -0,0 +1,48 @@
+import React from "react";
+import "./Cards.css";
+import img from "../../assets/images/03_Favourite_blank/Group38/group/icon_nothing.png";
+import likeactive from "../../assets/images/02_Home_Favourite/Group/icon_favourite_Active.png";
+function CardMobile() {
+  return (
+    <div className="card-container">
+      <div className="top-text-div">
+        <p>6 city added to favourite </p>
+        <p>Remove all </p>
+      </div>
+      <div className="card">
+        <div className="card-innertext-div">
+          <p className="card-city-text">CIty,state</p>
+          <img src={likeactive} className="card-climate-img" />
+          <span className="card-temp-text"> 31 </span>
+          <span> °C </span>
+          <span className="card-climate-text"> Mostly sunny</span>
+        </div>
+        <img src={likeactive} className="card-likeactive-img" />
+      </div>
+      <div className="card"></div>
+    </div>
+  );
+}
+function CardWeb() {
+  return (
+    <div className="card-container">
+      <div className="top-text-div">
+        <p>6 city added to favourite </p>
+        <p>Remove all </p>
+      </div>
+      <div className="card">
+        <div className="card-innertext-div">
+          <p className="card-city-text">CIty,state</p>
+          <img src={likeactive} className="card-climate-img" />
+          <span className="card-temp-text"> 31 </span>
+          <span> °C </span>
+          <span className="card-climate-text"> Mostly sunny</span>
+        </div>
+        <img src={likeactive} className="card-likeactive-img" />
+      </div>
+      <div className="card"></div>
+    </div>
+  );
+}
+
+export { CardMobile, CardWeb };
--- src/pages/Favourite/favourite.js
+++ src/pages/Favourite/favourite.js
@@ -1,5 +1,19 @@
-function Favourite() {
-  return <div >favourite page</div>;
+import "./favourite.css";
+import img from "../../assets/images/03_Favourite_blank/Group38/group/icon_nothing.png";
+import { CardMobile, CardWeb } from "../../components/cards/Cards";
+
+function FavouriteMobile() {
+  return (
+    <div className="favcontainer">
+      {/* <div className="no-fav-div">
+        <img src={img}></img>
+        <p>No favourites added</p>
+      </div> */}
+
+      {/* <CardMobile/> */}
+      <CardWeb />
+    </div>
+  );
 }
 
-export default Favourite;
+export default FavouriteMobile;
